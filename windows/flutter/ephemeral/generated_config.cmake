52e300420cafe1e1b786cd6f4d2dfa1f72e53f36
Paul Carouge <113673467+HePaulTV@users.noreply.github.com>
2023-04-19 16:24:48 +0200

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "B:\\01 - SOFTWARE\\04 - Visual Studio Code\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "B:\\02 - PROJECTS\\01 - Developpement\\01 - Flutter\\Paradice" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=B:\\01 - SOFTWARE\\04 - Visual Studio Code\\Flutter\\flutter"
+  "PROJECT_DIR=B:\\02 - PROJECTS\\01 - Developpement\\01 - Flutter\\Paradice"
+  "FLUTTER_ROOT=B:\\01 - SOFTWARE\\04 - Visual Studio Code\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=B:\\02 - PROJECTS\\01 - Developpement\\01 - Flutter\\Paradice\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=B:\\02 - PROJECTS\\01 - Developpement\\01 - Flutter\\Paradice"
+  "FLUTTER_TARGET=B:\\02 - PROJECTS\\01 - Developpement\\01 - Flutter\\Paradice\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=B:\\02 - PROJECTS\\01 - Developpement\\01 - Flutter\\Paradice\\.dart_tool\\package_config.json"
+)
